a7355f1b0e341f819b0466934e2ac5172dfb1596
drodriguez97 <danrod.biz@gmail.com>
2024-03-01 21:39:45 -0700

    Upgraded App.jsx with better organization

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,36 @@
-import { ChakraProvider, VStack} from '@chakra-ui/react';
-import { Header, Footer } from './components/HeaderFooter';
+import { ChakraProvider, VStack, Center, Heading } from '@chakra-ui/react';
+import { Nav, Footer } from './components/NavFooter';
 import Login from './components/Login';
 import Profile from './components/Profile';
-import WorkoutSchedule from './components/WorkoutSchedule';
+import WorkoutScheduleButton from './components/WorkoutScheduler';
 
 export default function App() {
     return (
         <ChakraProvider>
-            <VStack minH="100vh" w="100vw" bg="gray.100">
-                <Header />
-                <VStack spacing={8} mt={3}>
-                    <Login />
-                    <Profile />
-                </VStack>
-                <WorkoutSchedule />
-                <Footer /> 
-            </VStack>
+            <header>
+                <Center bg='blue.500' color='white' width='100%' padding={10}>
+                    <VStack>
+                        <Heading padding={4}>Gymstagram</Heading>
+                        <Nav />
+                    </VStack> 
+                </Center>
+            </header>
+
+            <main>
+                <Center bg='white.500' color='black' width='100%' padding={5}>
+                    <VStack spacing={50} paddingY={10}>
+                        <Profile />
+                        <Login />
+                        <WorkoutScheduleButton />
+                    </VStack> 
+                </Center>
+            </main>
+
+            <footer>
+                <VStack spacing={30} width='100%'>        
+                    <Footer />    
+                </VStack>   
+            </footer>       
         </ChakraProvider>
     );
 }
-
-
-
