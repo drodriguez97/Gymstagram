427719e6851bd901621936c03946c1d4aaeddd57
drodriguez97 <danrod.biz@gmail.com>
2024-03-01 21:42:37 -0700

    Fixed user backend request to Firebase to authenticate user login

--- Backend/routes/user_routes.py
+++ Backend/routes/user_routes.py
@@ -1,7 +1,8 @@
 import pyrebase 
 import creds
-from flask import Blueprint
-from flask import request, jsonify
+import secrets
+from flask import Flask, Blueprint
+from flask import request, jsonify, session
 
 user_bp = Blueprint('user_bp', __name__)
 
@@ -21,19 +22,22 @@ auth = firebase.auth()
 @user_bp.route('/profile', methods=["GET"])
 def my_profile():
     response_body = {
-        "name": "Nagato",
+        "name": "Daniel",
         "about" :"Hello! I'm a full stack developer that loves python and javascript"
     }
     return response_body
 
-@user_bp.route('/login', methods=["GET"])
+@user_bp.route('/login', methods=['POST', 'GET'])
 def login():
-    data = request.json
-    email = data.get('email')
-    password = data.get('password')
+    if request.method == 'POST':
+        data = request.json
+        email =data.get('email')
+        password = data.get('password')
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            return 'Login successful'  # Or any other success response
+        except Exception as e:
+            return str(e), 401 
     
-    try:
-        user = auth.sign_in_with_email_and_password(email, password)
-        return jsonify({'message': 'Authentification Successful', 'user': user}), 200
-    except Exception as e:
-        return jsonify({'message': 'Authentification failed', 'error': str(e)}), 401
+if __name__ == '__main__':
+    user_bp.run(debug=True)
