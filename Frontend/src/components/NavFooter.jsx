6484437e6cd3c9ff4add766d979572165e65a3ef
drodriguez97 <danrod.biz@gmail.com>
2024-03-01 21:39:01 -0700

    Re-did NavHeader and replaced with NavFooter

--- Frontend/src/components/NavFooter.jsx
+++ Frontend/src/components/NavFooter.jsx
@@ -1,12 +1,11 @@
-import { Box, Link, Flex } from '@chakra-ui/react';
+import { Box, Link, Flex, Text } from '@chakra-ui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
 
-export function Header() {
+export function Nav() {
     return (
-        <Box bg="blue.500" color="white">
+        <Box>
             <Flex justifyContent="center">
-                <Link px={4} py={2} _hover={{ textDecoration: 'none', bg: 'blue.600' }}>Gymstagram</Link>
                 <Link px={4} py={2} _hover={{ textDecoration: 'none', bg: 'blue.600' }}>
                     <FontAwesomeIcon icon={faHome} style={{ marginRight: '0.5rem' }} /> Home
                 </Link>
@@ -23,10 +22,10 @@ export function Header() {
 
 export function Footer() {
     return (
-        <Box bg="blue.500" color="white" mt="auto">
-            <Flex justifyContent="center">
-                <Box py={2}>© 2024 Gymstagram. All rights reserved.</Box>
-            </Flex>
+        <Box color="black" mt="auto">
+        <Flex justifyContent="center">
+            <Text fontSize="sm" color="gray.500">© 2024 Gymstagram. All rights reserved.</Text>
+        </Flex>
         </Box>
     );
 }
